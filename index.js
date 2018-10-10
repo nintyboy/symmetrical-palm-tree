e875fc01af22a40c852fdb0e577f4cec549456c4
Tyrell James <Nintyboy@gmail.com>
2018-10-10 12:17:05 +0100

    [FIX] Put IP finder in the correct place

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var ip = req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     (req.connection.socket ? req.connection.socket.remoteAddress : null);
 
-console.log("IP Address : " +ip);
 
 app.get('/', function(req,res){
+  var ip = req.headers['x-forwarded-for'] ||
+       req.connection.remoteAddress ||
+       req.socket.remoteAddress ||
+       (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+  console.log("IP Address : " +ip);
   res.send('Hello World! ' + keys.ENVIROMENT);
 });
 
