72a0ded08be407fcc70fdd74b94c5cf5bb138ecb
Tyrell James <Nintyboy@gmail.com>
2018-12-30 14:50:14 +0000

    Updated Firebase ENV Keys

--- config/staging.js
+++ config/staging.js
@@ -3,39 +3,15 @@ module.exports = {
 	ENVIROMENT: "This is Staging",
 	MERCURY_KEY: process.env.MERCURY_KEY,
 	FIREBASE: {
-		"type": process.env.FIREBASE.TYPE,
-		"project_id": process.env.FIREBASE.PROJECT_ID,
-		"private_key_id": process.env.FIREBASE.PROJECT_KEY_ID,
-		"private_key": process.env.FIREBASE.PRIVATE_KEY,
-		"client_email": process.env.FIREBASE.CLIENT_EMAIL,
-		"client_id": process.env.FIREBASE.CLIENT_ID,
-		"auth_uri": process.env.FIREBASE.AUTH_URI,
-		"token_uri": process.env.FIREBASE.TOKEN_URI,
-		"auth_provider_x509_cert_url": process.env.FIREBASE.AUTH_PROVIDER_X509_CERT_URL,
-		"client_x509_cert_url": process.env.FIREBASE.AUTH_X509_CERT_URL
-	},
-	GOOGLE_CLOUD_SPEECH: {
-		"type": process.env.GOOGLE_CLOUD_SPEECH.TYPE,
-		"project_id": process.env.GOOGLE_CLOUD_SPEECH.PROJECT_ID,
-		"private_key_id": process.env.GOOGLE_CLOUD_SPEECH.PROJECT_KEY_ID,
-		"private_key": process.env.GOOGLE_CLOUD_SPEECH.PRIVATE_KEY,
-		"client_email": process.env.GOOGLE_CLOUD_SPEECH.CLIENT_EMAIL,
-		"client_id": process.env.GOOGLE_CLOUD_SPEECH.CLIENT_ID,
-		"auth_uri": process.env.GOOGLE_CLOUD_SPEECH.AUTH_URI,
-		"token_uri": process.env.GOOGLE_CLOUD_SPEECH.TOKEN_URI,
-		"auth_provider_x509_cert_url": process.env.GOOGLE_CLOUD_SPEECH.AUTH_PROVIDER_X509_CERT_URL,
-		"client_x509_cert_url": process.env.GOOGLE_CLOUD_SPEECH.AUTH_X509_CERT_URL
-	},
-	GOOGLE_CLOUD_STORAGE: {
-		"type": process.env.GOOGLE_CLOUD_STORAGE.TYPE,
-		"project_id": process.env.GOOGLE_CLOUD_STORAGE.PROJECT_ID,
-		"private_key_id": process.env.GOOGLE_CLOUD_STORAGE.PROJECT_KEY_ID,
-		"private_key": process.env.GOOGLE_CLOUD_STORAGE.PRIVATE_KEY,
-		"client_email": process.env.GOOGLE_CLOUD_STORAGE.CLIENT_EMAIL,
-		"client_id": process.env.GOOGLE_CLOUD_STORAGE.CLIENT_ID,
-		"auth_uri": process.env.GOOGLE_CLOUD_STORAGE.AUTH_URI,
-		"token_uri": process.env.GOOGLE_CLOUD_STORAGE.TOKEN_URI,
-		"auth_provider_x509_cert_url": process.env.GOOGLE_CLOUD_STORAGE.AUTH_PROVIDER_X509_CERT_URL,
-		"client_x509_cert_url": process.env.GOOGLE_CLOUD_STORAGE.AUTH_X509_CERT_URL
+		"type": process.env.FIREBASE_TYPE,
+		"project_id": process.env.FIREBASE_PROJECT_ID,
+		"private_key_id": process.env.FIREBASE_PROJECT_KEY_ID,
+		"private_key": process.env.FIREBASE_PRIVATE_KEY,
+		"client_email": process.env.FIREBASE_CLIENT_EMAIL,
+		"client_id": process.env.FIREBASE_CLIENT_ID,
+		"auth_uri": process.env.FIREBASE_AUTH_URI,
+		"token_uri": process.env.FIREBASE_TOKEN_URI,
+		"auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+		"client_x509_cert_url": process.env.FIREBASE_AUTH_X509_CERT_URL
 	}
 }
